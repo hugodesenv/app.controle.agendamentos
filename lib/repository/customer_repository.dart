f0e978add1d5a632133066f5d9ee2b4249ca6f71
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2023-05-17 13:55:56 -0300

    Padronizaçoes

--- lib/repository/customer_repository.dart
+++ lib/repository/customer_repository.dart
@@ -2,27 +2,19 @@ import 'package:agendamentos/model/customer.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CustomerRepository {
-  final String _collection = 'person';
+  final _fireCloud = FirebaseFirestore.instance.collection('person');
 
   CustomerRepository._();
 
   static final instance = CustomerRepository._();
 
   Future save(Customer customer) async {
-    var db = FirebaseFirestore.instance;
-
-    await db
-        .collection(_collection)
-        .doc()
-        .set(customer.toMap())
-        .then((value) {})
-        .onError((error, stackTrace) {});
+    await _fireCloud.doc().set(customer.toMap()).then((value) {}).onError((error, stackTrace) {});
   }
 
   Future<List<Customer>> fetchData() async {
     List<Customer> customers = [];
-    var db = FirebaseFirestore.instance;
-    var collections = await db.collection(_collection).get();
+    var collections = await _fireCloud.get();
 
     for (var doc in collections.docs) {
       Customer customer = Customer.fromJson(doc.data());
