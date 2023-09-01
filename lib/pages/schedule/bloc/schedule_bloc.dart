7c5db90298f76949510f23cfb3e12d18b7618c14
Hugo Souza <hugo.souza@mbmsolutions.com.br>
2023-08-31 21:25:18 -0300

    Coisas

--- lib/pages/schedule/bloc/schedule_bloc.dart
+++ lib/pages/schedule/bloc/schedule_bloc.dart
@@ -3,11 +3,11 @@ import 'package:agendamentos/pages/schedule/bloc/schedule_state.dart';
 import 'package:bloc/bloc.dart';
 
 class ScheduleBloc extends Bloc<ScheduleEvent, ScheduleState> {
-  ScheduleBloc(super.initialState) {
-    on<ScheduleEventLoad>(_load);
+  ScheduleBloc(super.initialStatge) {
+    on<ScheduleEventFindAll>(_findAll);
   }
 
-  Future<void> _load(ScheduleEventLoad event, emit) async {
-
+  Future<void> _findAll(ScheduleEventFindAll event, emit) async {
+    // buscar os dados da api aqui...
   }
 }
--- lib/pages/schedule/bloc/schedule_event.dart
+++ lib/pages/schedule/bloc/schedule_event.dart
@@ -1,5 +1,3 @@
 abstract class ScheduleEvent {}
 
-class ScheduleEventLoad extends ScheduleEvent {
-
-}
+class ScheduleEventFindAll extends ScheduleEvent {}
